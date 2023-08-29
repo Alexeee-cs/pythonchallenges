665cb42af0eef4490e16237e887237a6ba24be2f
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-08-29 10:05:30 +0800

    Update TU10 pick a number .py
    Make a number generator for floats between 0.1 and 9.9
    
    Put it all together. Make a program that picks a name at random and then gives the user the choice to keep it in the list or not.

--- TU10 pick a number .py
+++ TU10 pick a number .py
@@ -1,5 +1,16 @@
 import random
-
 mynumber = random.uniform(0.1, 9.9)
 New_number = round(mynumber,3)
 print(New_number)
+
+
+import random
+Choose_Name = ["James","John","Mark","Rick"]
+for i in range (1,6):
+  chosen = random.choice(Choose_Name)
+  print(chosen)
+  choice = input("Do you want to remove the name? ")
+  if choice == ["y,Y,yes,Yes,YES"]:
+    Choose_Name.remove(chosen)
+  else:
+    print(" ")
