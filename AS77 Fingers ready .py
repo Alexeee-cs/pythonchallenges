29132ef58980aad8ce94943c988097a5b27229c7
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-09-11 08:23:24 +0800

    Create AS77 Fingers ready .py
    Try to work out what the output would be for each of these statements. You can guess as often as you need to, but try to do this without using the Python IDE.

---
+++ AS77 Fingers ready .py
@@ -0,0 +1,12 @@
+print ("Python"[1]) = y
+print("Programmer"[1]) = r
+print("This year"[5:]) = year
+print(len("will conquer")) = 12
+print("so better"[:2]) = so
+print("go and see a psychotherapist"[17:20]) = hot
+print("p" in "Pineapple") = True
+print("apple" == "Pineapple") = False
+print("pear" not in "Pineapple") = True
+print("pear" != "Pineapple") = True
+print("apple" > "pineapple") = False
+print("pineapple" < "Peach") = False
