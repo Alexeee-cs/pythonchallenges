9ebc9be2e62f4591d68791eb5965730913527495
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-09-25 08:35:33 +0800

    Create Data Generator .py
    Create functions to generate data in all of the pseudocode data types.

---
+++ Data Generator .py
@@ -0,0 +1,17 @@
+data_type = input("Enter data type from integer, real, char, boolean and string: ")
+
+if data_type == 'string':
+    print('string')
+if data_type == 'boolean':
+    stat = input("Is your statement true or not? ")
+    if stat == 'true':
+        print('true')
+    else:
+        print('false')
+if data_type == 'integer':
+    print('1')
+if data_type == 'real':
+    print('1.11')
+if data_type == 'char':
+   print(a)
+    
