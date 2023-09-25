b7ac9badee7b89f5f45e41d06d920858c241d46c
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-09-25 08:03:23 +0800

    Create GC45 VALIDATE A CHARACTER .py
    Use validation to check that the user has entered a single character.

---
+++ GC45 VALIDATE A CHARACTER .py
@@ -0,0 +1,10 @@
+while True:
+    user_input = input('Enter a single character: ')
+
+    if len(user_input) == 1:
+        print(user_input.upper())
+        break
+
+    else:
+        print('Enter a single character to continue.')
+        break
