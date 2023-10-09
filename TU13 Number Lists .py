8f6f9e4ec207f9065d782cbf6fb11527c6bded34
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-10-09 00:23:38 +0000

    Create TU13 Number Lists .py
    Exercise 1: Create a list of number and total it up.
    
    EXERCISE 2: Create a list's vital statistics including total, minimum, maxiumum, length & mean average

---
+++ TU13 Number Lists .py
@@ -0,0 +1,10 @@
+ylist = [19,28,12,1,15,8,13,9]
+print(sum(Mylist))
+
+
+Mylist = [19,28,12,1,15,8,13,9]
+print(sum(Mylist))
+print(min(Mylist))
+print(max(Mylist))
+print(len(Mylist))
+print(sum(Mylist)/len(Mylist))
