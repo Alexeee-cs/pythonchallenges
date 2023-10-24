ccca58c0f6abc7815635047c48846094d3e08f2a
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-10-24 09:06:02 +0800

    Create GC05 DEMO ALGORITHMS .py
    Create demo code to demonstrate the ideas of: sequence, selection, repetition & variable.

---
+++ GC05 DEMO ALGORITHMS .py
@@ -0,0 +1,24 @@
+#Sequence
+print("H")
+print("E")
+print("L")
+print("L")
+print("O\n")
+
+#Selection
+a = 66
+b = int(input("any number: "))
+if a>b:
+    print(a)
+else:
+    print(b)
+print('\n')
+
+#Iteration
+print("Hello"*3,sep=',')
+print('\n')
+
+#Variable
+x = "hello"
+y = "john"
+print(x,y)
