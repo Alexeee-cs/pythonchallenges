d24e3c74f115e3f443a0be72d6412d1b2fa9a93a
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-10-24 08:19:26 +0800

    Update AS02 Linear Search .py

--- AS02 Linear Search .py
+++ AS02 Linear Search .py
@@ -1,14 +1,14 @@
 def search(list,target):
     for i in range(len(list)):
-      if list[i] == target:
-        return i
-    return False
+        if list[i] == target:
+            return i
+    return 'False'
 
 list = [5,8,4,6,9,2]
 target = int(input("Please input your target number: "))
 position = search(list,target)
 
-if position!=False:
-   print("Found at position",position)
+if position!='False':
+   print("Found at position",position+1)
 else:
    print("Not Found")
