1eb6efc91627816dfc72cca9bc44ca34121e7bd3
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-10-24 08:39:12 +0800

    Create TU03 Printing String and Number .py
    Exercise 1: Tell the person that MyAge "Is a very nice age to be"
    
    Exercise 2: Change the code so that the computer has an age to 3 decimal places and is older than you.

---
+++ TU03 Printing String and Number .py
@@ -0,0 +1,9 @@
+ComputerAge=3
+MyAge = int(input('What is your age?'))
+print('%d is a very nice age to be' % (MyAge))
+
+
+ComputerAge=99.87654321
+ComputerName='Mr.Computer'
+MyAge = int(input('What is your age?'))
+print('I am older than %d, because I am %.3f My name is %s.' % (MyAge,ComputerAge,ComputerName))
