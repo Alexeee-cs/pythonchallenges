69c07e3c125bb33d03308833baf37c43e9e84e4a
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-10-30 16:44:08 +0800

    Update AS08 Rock, paper, scissor .py

--- AS08 Rock, paper, scissor .py
+++ AS08 Rock, paper, scissor .py
@@ -1,1 +1,31 @@
+import random
+action = ['rock','paper','scissor']
+decision = random.choice(action)
+user_action = input("Please choose your move (rock/paper/scissor): ")
+user_move = user_action.lower()
+if user_move == 'rock':
+    if decision == 'rock':
+        print('tie')
+    elif decision == 'paper':
+        print('you lost')
+    else:
+        print('you win')
+elif user_move == 'paper':
+    if decision == 'rock':
+        print('you win')
+    elif decision == 'paper':
+        print('tie')
+    else:
+        print('you lost')
+elif user_move == 'scissor':
+    if decision == 'rock':
+        print('you lost')
+    elif decision == 'paper':
+        print('you win')
+    else:
+        print('tie')
+else:
+    print("INVALID move")
 
+print("The computer played:",decision)
+print("You played:",user_move)
