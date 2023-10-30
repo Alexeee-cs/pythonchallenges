448aead3f55293e19c5a565515e63bed9cc204d2
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-10-30 16:53:34 +0800

    Update AS07 Caesar cipher .py

--- AS07 Caesar cipher .py
+++ AS07 Caesar cipher .py
@@ -1,1 +1,9 @@
+sentence = input("Enter a string: ")
+new_sentence = ''
+for char in sentence:
+    new_char = ord(char) +1
+    if new_char == 33:
+        new_char = 32
+    new_sentence += chr(new_char)
 
+print(new_sentence)
