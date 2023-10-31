70c5edf862e26938fd958896d55927fff9b41b94
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-10-31 21:12:17 +0800

    Update AS11 Pass Phrase Generetor .py

--- AS11 Pass Phrase Generetor .py
+++ AS11 Pass Phrase Generetor .py
@@ -1,1 +1,9 @@
+import random
+number = ['','','','','','','','','','']
+password = ''
 
+for i in range(len(number)):
+    number[i] = random.randint(0,256)
+    password += chr(number[i])
+
+print(password)
