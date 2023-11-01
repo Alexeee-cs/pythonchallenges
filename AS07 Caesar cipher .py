86ec5fe12d5fe894af8978cec746491c6dfb6687
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-11-01 17:27:42 +0800

    Update AS07 Caesar cipher .py

--- AS07 Caesar cipher .py
+++ AS07 Caesar cipher .py
@@ -1,9 +1,13 @@
 sentence = input("Enter a string: ")
 new_sentence = ''
-for char in sentence:
-    new_char = ord(char) +1
-    if new_char == 33:
+for char in sentence.lower():
+    if ord(char) == 32:
         new_char = 32
+    else:
+        new_char = ord(char) + 10
+        if new_char >= 122:
+            temp = new_char - 122
+            new_char = 97 + temp
     new_sentence += chr(new_char)
 
 print(new_sentence)
