ceabfc9552d9dbd5a778405d6de38a0166104cac
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2023-11-01 17:47:00 +0800

    Update AS07 Caesar cipher .py

--- AS07 Caesar cipher .py
+++ AS07 Caesar cipher .py
@@ -5,9 +5,9 @@ for char in sentence.lower():
         new_char = 32
     else:
         new_char = ord(char) + 10
-        if new_char >= 122:
+        if new_char > 122:
             temp = new_char - 122
-            new_char = 97 + temp
+            new_char = 96 + temp
     new_sentence += chr(new_char)
 
 print(new_sentence)
