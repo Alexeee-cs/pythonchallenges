c25194fa2224bdd0cf6fbbd7e0c5b9fb723ffc87
Alexeee-cs <143363108+Alexeee-cs@users.noreply.github.com>
2024-10-28 11:47:09 +0800

    Update OB01 DadJokes .py

--- OB01 DadJokes .py
+++ OB01 DadJokes .py
@@ -1,9 +1,4 @@
 import random
-class Jokes():
-    def __init__(self):
-       self.prompt = ""
-       self.answer = ""
-
 jokes = []
 def get_jokes():
     try:
@@ -14,7 +9,25 @@ def get_jokes():
         print("Sorry couldn't find the file")
 
 get_jokes()
-num = random.randint(0,(len(jokes)//2))
-print(jokes[2*num])
-response = input("Enter the answer: ")
-print(jokes[2*num+1])
+
+class Jokes():
+    def __init__(self):
+       self.__prompt = ""
+       self.__answer = ""
+
+    def setter(self):
+        num = random.randint(0,10)
+        self.__prompt = jokes[2*num]
+        self.__answer = jokes[2*num+1]
+
+    def prompt_getter(self):
+        return self.__prompt
+
+    def answer_getter(self):
+        return self.__answer
+
+joke = Jokes()
+joke.setter()
+print("Joke:", joke.prompt_getter())
+response = input("Your response: ")
+print("Answer:", joke.answer_getter())
